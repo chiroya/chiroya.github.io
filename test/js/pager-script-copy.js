049c925d989de1052c477f484cda419527fb05e7
chiroya <chiroyawn@gmail.com>
2024-02-23 04:47:02 +0900

    240222 Gallery swiper module

---
+++ test/js/pager-script-copy.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const swiperEl = document.querySelector('.mySwiper');
+    Object.assign(swiperEl, {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            clickable: true,
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+            },
+        },
+    });
+    swiperEl.initialize();
+});
