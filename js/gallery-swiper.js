e5f30dbde960e79010d3090542d0d90a1942fcee
chiroya <chiroyawn@gmail.com>
2024-10-07 03:52:37 +0900

    241007 Gallery Swiper Module Update

--- js/gallery-swiper.js
+++ js/gallery-swiper.js
@@ -2,19 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // 이미지 경로 배열
     const images = [
-        'https://qrcodethumb-phinf.pstatic.net/20230504_82/16832049920215qGVn_PNG/%EC%95%BC%EC%9E%98%EC%A1%B0%EC%95%84%EC%9A%94(arca).png', 
-        'https://qrcodethumb-phinf.pstatic.net/20230504_175/1683204991270Fjv51_PNG/%EC%82%90%EC%9A%94-%EB%A7%88%EB%8F%84_ai-painter.png', 
-        'https://qrcodethumb-phinf.pstatic.net/20230520_280/168455967628507XgR_JPEG/%EB%8B%88%EB%82%98%EC%A7%B1-SD.jpg', 
-        'https://qrcodethumb-phinf.pstatic.net/20230504_198/1683204990963tCeek_PNG/NANKAM_%EB%8B%89%EB%84%B4x.png', 
-        'https://qrcodethumb-phinf.pstatic.net/20230504_28/1683204992476mUfvX_PNG/%EC%9E%A1%EC%B4%88%EC%96%91_%EC%A0%84%EC%8B%A0_%ED%9D%B0%EB%B0%B0%EA%B2%BD.png', 
-        'https://qrcodethumb-phinf.pstatic.net/20230504_174/1683204991769Hp9QD_JPEG/%EC%8B%B8%EB%A3%A8_%EB%85%B8%EC%9D%84.jpg', 
-        'https://qrcodethumb-phinf.pstatic.net/20230504_112/1683204991595v7mNA_PNG/%EC%82%AC%EC%BF%A0%EC%97%90_%EC%88%98%EC%A0%95%EB%B3%B8(%EC%B5%9C%EC%A2%85).png', 
-        'https://qrcodethumb-phinf.pstatic.net//20241007_184/1728233510335oQBAE_PNG/%BD%BA%C6%BC%BA%EC%B9%E9.png', 
-        'https://qrcodethumb-phinf.pstatic.net//20240213_253/1707763035002FH3cM_PNG/%C5%E4%C7%C7%B3%D3%B6%F3%B6%BC.png', 
-        'https://qrcodethumb-phinf.pstatic.net/20230504_65/1683204992257X4Dpk_PNG/%EC%9E%A1%EC%B4%88%EC%96%91_%EB%B0%98%EC%8B%A0_%ED%9D%B0%EB%B0%B0%EA%B2%BD.png', 
-        'https://qrcodethumb-phinf.pstatic.net/20230504_217/1683204992648HdCDH_JPEG/ha4444.jpg'
+        'https://qrcodethumb-phinf.pstatic.net//20241007_46/1728240478011djFBK_PNG/011.png', 
+        'https://qrcodethumb-phinf.pstatic.net//20241007_14/1728240478060mQeJ3_JPEG/002.jpg', 
+        'https://qrcodethumb-phinf.pstatic.net//20241007_15/1728240478055YrWBw_PNG/012.png', 
+        'https://qrcodethumb-phinf.pstatic.net//20241007_297/1728240478080Wz4Gm_JPEG/003.jpg', 
+        'https://qrcodethumb-phinf.pstatic.net//20241007_262/1728240478045UsPGJ_PNG/005.png', 
+        'https://qrcodethumb-phinf.pstatic.net//20241007_176/1728240477995tRhcw_PNG/010.png', 
+        'https://qrcodethumb-phinf.pstatic.net//20241007_221/1728240477970aWmVq_PNG/006.png', 
+        'https://qrcodethumb-phinf.pstatic.net//20241007_230/1728240478068j098G_PNG/008.png', 
+        'https://qrcodethumb-phinf.pstatic.net//20241007_240/1728240477972FyiLG_PNG/004.png', 
+        'https://qrcodethumb-phinf.pstatic.net//20241007_100/1728240478010xTgNN_PNG/009.png', 
+        'https://qrcodethumb-phinf.pstatic.net//20241007_224/17282404779570lF8X_JPEG/007.jpg'
     ]
-
 
 
 
